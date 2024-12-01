ee2b9ca931263d5e42ef6e6019d446c9ab6f8261
Ramajayam Kasi <ramajayamkasi62@gmail.com>
2024-12-01 23:04:58 +0530

    Set up

---
+++ js/barcode_type_details.js
@@ -0,0 +1,77 @@
+var barcode_type_details = {
+  CODE39: `<ol>
+<li>Code 39 is a popular type of barcode that is widely used in various industries for labeling purposes.</li>
+<li>It is a variable length barcode that can encode up to 43 alphanumeric characters at one time. </li>
+<li>Example barcode data: yhgwh76378</li>
+</ol>`,
+  CODE128: `<ol>
+<li>CODE128 is a type of barcode that is widely used in various industries due to its high data density and versatility.</li>
+<li>CODE128 can encode all 128 ASCII characters, including numbers, letters (both uppercase and lowercase), symbols, and control characters.</li>
+<li>Example barcode data: yhgwh76378</li>
+</ol>`,
+  EAN13: `<ol>
+<li>The EAN-13 (European Article Number 13) is a type of barcode used globally for product identification.</li>
+<li>Length: An EAN-13 barcode consists of 12 digits.</li>
+<li>Example barcode data: 5901234123457</li>
+</ol>`,
+  EAN8: `<ol>
+<li>EAN-8 (European Article Number 8) is a type of barcode commonly used for labeling small products like candies, cigarettes, and other small items.</li>
+<li>An EAN-8 barcode consists of 8 digits.</li>
+<li>Example barcode data: 59012341</li> 
+</ol>`,
+  EAN5: `<ol>
+<li>EAN-5 is a variant of the European Article Number (EAN) barcode system.</li>
+<li>An EAN-5 barcode consists of 5 digits.</li>
+<li>Example barcode data: 59012</li>
+</ol>`,
+  EAN2: `<ol>
+<li>EAN-2 is a subset of the European Article Number (EAN) barcode system. It's a two-digit supplementary barcode used primarily for magazine and newspaper distribution.</li>
+<li>An EAN-2 barcode consists of 2 digits.</li>
+<li>Example barcode data: 59</li>
+</ol>`,
+  UPC: `<ol>
+<li>UPC-A is a type of barcode commonly used in the United States and Canada for retail products.</li>
+<li>An UPC-A barcode consists of 12 digits.</li>
+<li>Example barcode data: 595794693621</li>
+</ol>`,
+  "UPC-A": `<ol>
+<li>UPC-A is a type of barcode commonly used in the United States and Canada for retail products.</li>
+<li>An UPC-A barcode consists of 12 digits.</li>
+<li>Example barcode data: 595794693621</li>
+</ol>`,
+  ITF14: `<ol>
+<li>ITF-14, also known as Interleaved 2 of 5, is a type of barcode used primarily for packaging and shipping purposes.</li>
+<li>An ITF-14 barcode consists of 13 digits.</li>
+<li>Example barcode data: 5957946936215036</li>
+</ol>`,
+  ITF: `<ol>
+<li>ITF (Interleaved 2 of 5) is a numeric-only barcode symbology. It's called "interleaved" because it encodes data in pairs of digits, with each digit represented by five bars (two wide and three narrow).</li>
+<li>ITF encodes data in pairs of digits, where each pair represents one character. The first digit is encoded using five bars, and the second digit is encoded using five spaces. This interleaved pattern gives ITF its name.</li>
+<li>Example barcode data: 595798</li>
+</ol>`,
+  MSI: `<ol>
+<li>An MSI barcode, also known as MSI Plessey or Modified Plessey, is a type of linear barcode symbology primarily used in inventory and warehouse management, particularly in the retail and library sectors.</li>
+<li>Character Set: Encodes numeric data only (digits 0-9).</li>
+<li>Example barcode data: 123456</li>
+</ol>`,
+  MSI10: `<ol>
+<li>The MSI10 barcode, also known as the MSI Plessey or Modified Plessey barcode, is a type of linear barcode that was developed in the 1970s for retail and inventory purposes.</li>
+<li>Numeric Only: The MSI10 barcode encodes only numeric digits (0-9), which makes it simpler but less versatile than some other barcode formats that support alphanumeric characters.</li>
+<li>Example barcode data: 1234567890</li>
+</ol>`,
+  MSI11: `<ol>
+<li>The term "MSI11 barcode" does not refer to a standard or widely recognized barcode format. It appears there might be some confusion or a typo in the name. The more commonly known barcode format is MSI (also known as MSI Plessey), which is a type of numeric-only barcode.</li>
+<li>Character Set:The MSI barcode is numeric-only, meaning it encodes only digits from 0 to 9.</li>
+<li>Example barcode data: 1 2 3 4 5 6</li>
+</ol>`,
+  MSI1010: `<ol>
+<li>An MSI barcode, also known as MSI Plessey or Modified Plessey, is a type of linear barcode symbology primarily used in inventory and warehouse management, particularly in the retail and library sectors.</li>
+<li>Character Set: Encodes numeric data only (digits 0-9).</li>
+<li>Example barcode data: 123456</li>
+</ol>`,
+  MSI1110: `<ol>
+<li>An MSI barcode, also known as MSI Plessey or Modified Plessey, is a type of linear barcode symbology primarily used in inventory and warehouse management, particularly in the retail and library sectors.</li>
+<li>Character Set: Encodes numeric data only (digits 0-9).</li>
+<li>Example barcode data: 123456</li>
+</ol>`,
+};
